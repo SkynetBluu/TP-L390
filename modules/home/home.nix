73ba72bd62d78e1352eb626c3c9a26a68d1822bc
nimbus <redactedusername@users.noreply.github.com>
2026-04-30 21:53:55 +0100

    revert windowrule to windowrulev2

--- modules/home/home.nix
+++ modules/home/home.nix
@@ -273,7 +273,7 @@
         ",XF86AudioPrev, exec, playerctl prev"
       ];
 
-      windowrule = [
+      windowrulev2 = [
         "workspace 2, class:^(brave-browser)$"
         "float, class:^(org.gnome.Nautilus)$"
         "size 900 600, class:^(org.gnome.Nautilus)$"
